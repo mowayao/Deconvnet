a6b425da3338d19959a1d64f79f4746312f4b7ba
mowayao <493745925@qq.com>
2018-01-24 13:26:34 +0800

    "update"

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ import cv2
 import numpy as np
 from torch.autograd import Variable
 import torch
+import torch.nn as nn
 from models.vgg16_conv_deconv import VGG_Conv, VGG_Deconv, Identity
-img = cv2.imread("cat.jpg")
+img = cv2.imread("dog.jpeg")
 img = cv2.resize(img, (224, 224)) / 255.
 
 
@@ -12,11 +13,13 @@ INPUT = np.transpose(img, [2, 0, 1])
 INPUT = Variable(torch.FloatTensor([INPUT])).cuda()
 
 conv_net = VGG_Conv().cuda()
-deconv_net = VGG_Deconv(Identity).cuda()
+conv_net.eval()
+deconv_net = VGG_Deconv(nn.ReLU).cuda()
+deconv_net.eval()
 out = conv_net(INPUT)
 
 conv_layer_idxes = [0, 2, 5, 7, 10, 12, 14, 17, 19, 21, 24, 26, 28]
-out = deconv_net(conv_net.outputs[14][0][66][None, None,:,:], 14, 66, conv_net.maxpool_idx).cpu().data.numpy()[0]
+out = deconv_net(conv_net.outputs[19][0][88][None, None,:,:], 19, 88, conv_net.maxpool_idx).cpu().data.numpy()[0]
 
 out = np.transpose(out, [1, 2, 0])
 out = (out-out.min()) /(out.max()-out.min()) * 255
